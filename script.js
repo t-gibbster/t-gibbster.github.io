357746f06ff4863885f53c3d05444ef15967132b
t-gibbster <troy.gibb@gmail.com>
2016-05-11 17:26:01 -0700

    Update script.js

--- script.js
+++ script.js
@@ -7,8 +7,8 @@ var day = currentDate.getDate()
 var month = currentDate.getMonth() + 1
 var year = currentDate.getFullYear()
 
-  $(document).click(function(){
+
     $('tomorrow').append("<p>" + currentDate + "</p>");
-  });
+
   
 });
