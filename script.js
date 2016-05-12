d957055c81a88cb215e49408932288e2bc80fa01
t-gibbster <troy.gibb@gmail.com>
2016-05-11 17:00:41 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 $(document).ready(function(){
   
-  alert("Are we working?")
+  
+
+var currentDate = new Date(new Date().getTime() + 24 * 60 * 60 * 1000);
+var day = currentDate.getDate()
+var month = currentDate.getMonth() + 1
+var year = currentDate.getFullYear()
+
+  $('tomorrow').click(function(){
+    $('tomorrow').append("<p>" + currentDate + "</p>");
+  });
   
 });
