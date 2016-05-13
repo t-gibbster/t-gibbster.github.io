368fcb51af4cbd2893246348b9cd55dc2bfb51f3
t-gibbster <troy.gibb@gmail.com>
2016-05-13 12:44:09 -0700

    Update script.js

--- script.js
+++ script.js
@@ -8,11 +8,11 @@ var x = function(result) {
 
 var y = $.get('https://api.genderize.io/?name=peter', x);
 
-var xhr2 = new XMLHttpRequest();
+//var xhr2 = new XMLHttpRequest();
 
-xhr2.open("GET", "https://api.meetup.com/find/locations?&sign=true&photo-host=public&query=94606", false);
-xhr2.send();
+//xhr2.open("GET", "https://api.meetup.com/find/locations?&sign=true&photo-host=public&query=94606", false);
+//xhr2.send();
 
-$('#cityInfo').append(xhr2.status, xhr.statusText)
+//$('#cityInfo').append(xhr2.status, xhr.statusText)
 
 });
