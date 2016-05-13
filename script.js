5cee6987fcae46b2be324f61a2c329bedb4c90a6
t-gibbster <troy.gibb@gmail.com>
2016-05-13 12:33:57 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 $(document).ready(function(){
   
-alert("hello?")  
+var xhr = new XMLHttpRequest();
 
-var currentDate = new Date(new Date().getTime() + 24 * 60 * 60 * 1000);
-var day = currentDate.getDate()
-var month = currentDate.getMonth() + 1
-var year = currentDate.getFullYear()
+xhr.open("GET", "http://www.codecademy.com/", false);
+xhr.send();
+
+console.log(xhr.status);
+console.log(xhr.statusText);
+
+$('cityInfo').append(xhr.status, xhr.statusText)
 
 
-    $('tomorrow').append("<p>" + currentDate + "</p>");
-
-  
 });
