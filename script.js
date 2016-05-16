eb7e0556c97b425c4f6f53dcdf79b17164f96c0a
t-gibbster <troy.gibb@gmail.com>
2016-05-16 11:25:44 -0700

    Update script.js

--- script.js
+++ script.js
@@ -5,8 +5,7 @@ var displayGroups = function(result) {
     $('#demo').append(result)
   };
 
-var y = $.get("https://api.meetup.com/find/locations?photo-host=public&query=94606&sig_id=182898232&sig=ffc85a4fa1e2ccc36b5936a85be2a84028c9c7ab
-", displayGroups);
+var y = $.get("https://api.meetup.com/find/locations?photo-host=public&query=94606&sig_id=182898232&sig=ffc85a4fa1e2ccc36b5936a85be2a84028c9c7ab", displayGroups);
 
 });
 
