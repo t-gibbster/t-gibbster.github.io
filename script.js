91c1955864fdb71271485a601e3bda953daa3dde
t-gibbster <troy.gibb@gmail.com>
2016-05-16 11:16:26 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,65 +1,11 @@
 $(document).ready(function(){
 
 
-var x = function(result) {
+var displayGroups = function(result) {
   
-  $('#demo').append(result.gender)
+  $('#demo').append(result)
   
 };
 
-var y = $.get('https://api.genderize.io/?name=peter', x);
-
-//var xhr2 = new XMLHttpRequest();
-
-//xhr2.open("GET", "https://api.meetup.com/find/locations?&sign=true&photo-host=public&query=94606", false);
-//xhr2.send();
-
-//$('#cityInfo').append(xhr2.status, xhr.statusText)
-
 });
 
-/*
-Google API's js client library, automatically calls on ClientLoad()
- <script src="https://apis.google.com/js/client.js?onload=onClientLoad" type="text/javascript"></script>
- 
-// Your use of the YouTube API must comply with the Terms of Service:
-// https://developers.google.com/youtube/terms
-
-// Helper function to display JavaScript value on HTML page.
-function showResponse(response) {
-    var responseString = JSON.stringify(response, '', 2);
-    document.getElementById('response').innerHTML += responseString;
-}
-
-// Called automatically when JavaScript client library is loaded.
-function onClientLoad() {
-    gapi.client.load('youtube', 'v3', onYouTubeApiLoad);
-}
-
-// Called automatically when YouTube API interface is loaded (see line 9).
-function onYouTubeApiLoad() {
-    // This API key is intended for use only in this lesson.
-    // See https://goo.gl/PdPA1 to get a key for your own applications.
-    gapi.client.setApiKey('AIzaSyCR5In4DZaTP6IEZQ0r1JceuvluJRzQNLE');
-
-    search();
-}
-
-function search() {
-    // Use the JavaScript client library to create a search.list() API call.
-    var request = gapi.client.youtube.search.list({
-        part: 'snippet',
-        q: "rock climbing"
-    });
-    
-    // Send the request to the API server,
-    // and invoke onSearchRepsonse() with the response.
-    request.execute(onSearchResponse);
-}
-
-// Called automatically with the response of the YouTube API request.
-function onSearchResponse(response) {
-    showResponse(response);
-}
-
-*/
